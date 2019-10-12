c9f9f8e5929a18d5ed5c8f5691f7537e68ded19b
Pietrox <szymanowski.p@gmail.com>
2019-10-12 10:51:41 +0200

    Small adjustment

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@nestjs/common';
 
 @Injectable()
 export class ApplicationService {
-	static getData(): Message {
+	getData(): Message {
 		return {message: 'Welcome to api!'};
 	}
 }
